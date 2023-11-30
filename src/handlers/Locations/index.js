25ed0ddf8270c593c38a728fac2bcd5f43b866f2
Hassaan Mujtaba <saddiquehari2511@gmail.com>
2023-11-30 14:25:36 -0800

    Get halqas of maqam and division api is created

--- src/handlers/Locations/index.js
+++ src/handlers/Locations/index.js
@@ -1,8 +1,16 @@
-const District = require('./District');
-const Division = require('./Division');
-const Halqa = require('./Halqa');
-const Maqam = require('./Maqam');
-const Province = require('./Province');
-const Tehsil = require('./Tehsil');
-
-module.exports = { District, Division, Halqa, Maqam, Province, Tehsil };
+const District = require("./District");
+const Division = require("./Division");
+const Halqa = require("./Halqa");
+const Maqam = require("./Maqam");
+const Province = require("./Province");
+const Tehsil = require("./Tehsil");
+const FilterHalqas = require("./FilterHalqas");
+module.exports = {
+  District,
+  Division,
+  Halqa,
+  Maqam,
+  Province,
+  Tehsil,
+  FilterHalqas,
+};
