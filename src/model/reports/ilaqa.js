f308a2c993f93d63bf547e71b405a27327749136
Shuja Ur Rehman <71079854+sabeeharif@users.noreply.github.com>
2024-04-30 18:38:09 +0500

    schema is created for the ilaqa

---
+++ src/model/reports/ilaqa.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const ilaqaReportModel = mongoose.Schema(
+  {
+    comments: {
+      type: String,
+      required: false,
+    },
+    month: {
+      type: Date,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    ilaqaAreaId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "Ilaqa",
+    },
+    maqamTanzeemId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "MaqamTanzeem",
+    },
+    wiId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "WorkerInfo",
+    },
+    maqamActivityId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "MaqamActivities",
+    },
+    mentionedActivityId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "MentionedActivities",
+    },
+    otherActivityId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "OtherActivities",
+    },
+    tdId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "ToseeDawat",
+    },
+    maqamDivisionLibId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "MaqamDivisionLibrary",
+    },
+    paighamDigestId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "PaighamDigest",
+    },
+    rsdId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "RozShabBedari",
+    },
+  },
+  { timestamps: true }
+);
+
+const IlaqaReportModel = mongoose.model("IlaqaReport", ilaqaReportModel);
+
+module.exports = { IlaqaReportModel };
