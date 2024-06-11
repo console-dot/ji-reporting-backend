592065c13ceb0c1e809139ea23c4058763cb5c5e
Hassaan Mujtaba <hassaanmujtaba1@gmail.com>
2024-06-11 19:42:05 +0500

    Api created and all data calculated in Api

--- src/handlers/Compilation.js
+++ src/handlers/Compilation.js
@@ -1,116 +1,242 @@
 const { ProvinceReportModel, MarkazReportModel, MaqamReportModel, DivisionReportModel, IlaqaReportModel, HalqaReportModel } = require("../model/reports");
 const Response = require("./Response");
-function sumObjectsInArray(array) {
-    if (array.length === 0) return {}; 
-
-    const sumObject = {};
-
-    array.forEach(obj => {
-        Object.keys(obj).forEach(key => {
-            if (isNaN(obj[key]) || key.startsWith('_')) return;
-
-            if (!(key in sumObject)) {
-                sumObject[key] = obj[key];
-            } else {
-                sumObject[key] += obj[key];
-            }
-        });
-    });
-
-    return sumObject;
+function splitKeysAndRemoveFirst(obj) {
+  const result = {};
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      const parts = key.split('-');
+      const newKey = parts.slice(1).join('-');
+      result[newKey] = obj[key];
+    }
+  }
+  return result;
 }
+function sumObjectsInArray(array) {
+  if (array.length === 0) return {};
+
+  const sumObject = {};
+
+  array.forEach(obj => {
+      Object.keys(obj).forEach(key => {
+          const value = obj[key];
+
+          if (typeof value === 'object' && value !== null) {
+              const nestedSum = sumObjectsInNestedObject(value, key, 1); // Start recursion depth at 1
+              for (const nestedKey in nestedSum) {
+                  const newKey = nestedKey;
+                  if (!(newKey in sumObject)) {
+                      sumObject[newKey] = nestedSum[newKey];
+                  } else {
+                      sumObject[newKey] += nestedSum[newKey];
+                  }
+              }
+          } else if (typeof value === 'number') {
+              if (!(key in sumObject)) {
+                  sumObject[key] = value;
+              } else {
+                  sumObject[key] += value;
+              }
+          }
+      });
+  });
+
+  return sumObject;
+}
+
+function sumObjectsInNestedObject(obj, parentKey = '', depth = 1) {
+  if (depth > 5) return {}; // Maximum recursion depth reached, return an empty object
+
+  const sum = {};
+  for (const key in obj) {
+      const value = obj[key];
+      const currentKey = parentKey ? `${parentKey}-${key}` : key;
+      if (typeof value === 'object' && value !== null) {
+          const nestedSum = sumObjectsInNestedObject(value, currentKey, depth + 1); // Increment depth
+          for (const nestedKey in nestedSum) {
+              const newKey = nestedKey;
+              if (!(newKey in sum)) {
+                  sum[newKey] = nestedSum[newKey];
+              } else {
+                  sum[newKey] += nestedSum[newKey];
+              }
+          }
+      } else if (typeof value === 'number') {
+          if (!(currentKey in sum)) {
+              sum[currentKey] = value;
+          } else {
+              sum[currentKey] += value;
+          }
+      }
+  }
+  return sum;
+}
+
 class Compilation extends Response {
     
-  getCompiledReports = async (req, res) => {
-    try {
-      const token = req.headers.authorization;
-      if (!token) {
-        return this.sendResponse(req, res, {
-          message: "Access Denied",
-          status: 401,
-        });
-      }
-      const areaId = req.params.id;
-      let startDate = req.query.startDate;
-      let endDate = req.query.endDate;
-      const areaType = req.query.areaType;
+    getCompiledReports = async (req, res) => {
+        try {
+            const token = req.headers.authorization;
+            if (!token) {
+                return this.sendResponse(req, res, {
+                    message: "Access Denied",
+                    status: 401,
+                });
+            }
+            const areaId = req.params.id;
+            let startDate = req.query.startDate;
+            let endDate = req.query.endDate;
+            const areaType = req.query.areaType;
 
-      if (!startDate || !endDate) {
-        return this.sendResponse(req, res, {
-          message: "Please provide both startDate and endDate.",
-          status: 400,
-        });
-      }
-      startDate = new Date(`${startDate}-01T00:00:00.000Z`);
-      endDate = new Date(`${endDate}-01T00:00:00.000Z`);
-      endDate.setMonth(endDate.getMonth() + 1);
-      console.log(areaType);
-      const query = {
-        month: {
-          $gte: startDate,
-          $lt: endDate,
-        }
-      };
-      let data;
-      switch(areaType) {
-        case 'province':
-            query.provinceAreaId = areaId;
-            data = await ProvinceReportModel.find(query);
-            break;
-        case 'markaz':
-            query.markazAreaId = areaId;
-            data = await MarkazReportModel.find(query);
-            break;
-        case 'maqam':
-            query.maqamAreaId = areaId;
-            console.log(query)
-            let a = await MaqamReportModel.find(query).populate([
-                { path: "maqamTanzeemId" },
-                { path: "wiId" },
-                { path: "maqamActivityId" },
-                { path: "mentionedActivityId" },
-                { path: "otherActivityId" },
-                { path: "tdId" },
-                { path: "muntakhibTdId" },
-                { path: "maqamDivisionLibId" },
-                { path: "paighamDigestId" },
-                { path: "rsdId" },
-                { path: "baitulmalId" },
-                { path: "collegesId" },
-                { path: "jamiaatId" },
-              ]);;
-              data=sumObjectsInArray(a);
-            break;
-        case 'division':
-            query.divisionAreaId = areaId;
-            data = await DivisionReportModel.find(query);
-            break;
-        case 'ilaqa':
-            query.ilaqaAreaId = areaId;
-            data = await IlaqaReportModel.find(query);
-            break;
-        case 'halqa':
-            query.halqaAreaId = areaId;
-            data = await HalqaReportModel.find(query);
-            break;
-        default:
+            if (!startDate || !endDate) {
+                return this.sendResponse(req, res, {
+                    message: "Please provide both startDate and endDate.",
+                    status: 400,
+                });
+            }
+
+          
+            let startDateObj, endDateObj;
+            if (startDate.length === 4 && endDate.length === 4) { 
+                startDateObj = new Date(`${startDate}-01-01T00:00:00.000Z`);
+                endDateObj = new Date(`${endDate}-12-31T23:59:59.999Z`);
+            } else { 
+                startDateObj = new Date(`${startDate}-01T00:00:00.000Z`);
+                endDateObj = new Date(`${endDate}-01T00:00:00.000Z`);
+                endDateObj.setMonth(endDateObj.getMonth() + 1);
+            }
+
+            const query = {
+                month: {
+                    $gte: startDateObj,
+                    $lt: endDateObj,
+                }
+            };
+
+            let data;
+            switch(areaType) {
+                case 'province':
+                    query.provinceAreaId = areaId;
+                    let provinceReports = await ProvinceReportModel.find(query).populate([
+                      { path: "provinceTanzeemId" },
+                      { path: "provinceWorkerInfoId" },
+                      { path: "provinceActivityId" },
+                      { path: "mentionedActivityId" },
+                      { path: "otherActivityId" },
+                      { path: "tdId" },
+                      { path: "provinceDivisionLibId" },
+                      { path: "paighamDigestId" },
+                      { path: "rsdId" },
+                      { path: "collegesId" },
+                      { path: "jamiaatId" },
+                      { path: "baitulmalId" },
+                    ]).lean();
+                    data = sumObjectsInArray(provinceReports);
+                    break;
+                case 'country':
+                    
+                    let markazReports = await MarkazReportModel.find(query).populate([
+                      { path: "markazTanzeemId" },
+                      { path: "markazWorkerInfoId" },
+                      { path: "markazActivityId" },
+                      { path: "mentionedActivityId" },
+                      { path: "otherActivityId" },
+                      { path: "tdId" },
+                      { path: "markazDivisionLibId" },
+                      { path: "rsdId" },
+                      { path: "collegesId" },
+                      { path: "jamiaatId" },
+                      { path: "baitulmalId" },
+                    ]).lean();
+                    data = sumObjectsInArray(markazReports)
+                    break;
+                case 'maqam':
+                    query.maqamAreaId = areaId;
+                    let a = await MaqamReportModel.find(query).populate([
+                        { path: "maqamTanzeemId" },
+                        { path: "wiId" },
+                        { path: "maqamActivityId" },
+                        { path: "mentionedActivityId" },
+                        { path: "otherActivityId" },
+                        { path: "tdId" },
+                        { path: "muntakhibTdId" },
+                        { path: "maqamDivisionLibId" },
+                        { path: "paighamDigestId" },
+                        { path: "rsdId" },
+                        { path: "baitulmalId" },
+                        { path: "collegesId" },
+                        { path: "jamiaatId" },
+                    ]).lean();
+                    
+                    data = sumObjectsInArray(a);
+                    break;
+                case 'division':
+                    query.divisionAreaId = areaId;
+                    let divReprots = await DivisionReportModel.find(query).populate([
+                      { path: "maqamTanzeemId" },
+                      { path: "wiId" },
+                      { path: "divisionActivityId" },
+                      { path: "mentionedActivityId" },
+                      { path: "otherActivityId" },
+                      { path: "tdId" },
+                      { path: "maqamDivisionLibId" },
+                      { path: "paighamDigestId" },
+                      { path: "baitulmalId" },
+                      { path: "rsdId" },
+                      { path: "collegesId" },
+                      { path: "jamiaatId" },
+                    ]).lean();
+                    data = sumObjectsInArray(divReprots)
+                    break;
+                case 'ilaqa':
+                    query.ilaqaAreaId = areaId;
+                    let ilaqaReports = await IlaqaReportModel.find(query).populate([
+                      { path: "maqamTanzeemId" },
+                      { path: "wiId" },
+                      { path: "maqamActivityId" },
+                      { path: "mentionedActivityId" },
+                      { path: "otherActivityId" },
+                      { path: "tdId" },
+                      { path: "maqamDivisionLibId" },
+                      { path: "paighamDigestId" },
+                      { path: "baitulmalId" },
+                      { path: "rsdId" },
+                    ]).lean();
+                    data = sumObjectsInArray(ilaqaReports);
+                    break;
+                case 'halqa':
+                    query.halqaAreaId = areaId;
+                    let halqaReports = await HalqaReportModel.find(query).populate([
+                      { path: "wiId" },
+                      { path: "halqaActivityId" },
+                      { path: "otherActivityId" },
+                      { path: "tdId" },
+                      { path: "halqaLibId" },
+                      { path: "rsdId" },
+                      { path: "halqaAreaId" },
+                      { path: "baitulmalId" },
+                    ]).lean();
+                    data = sumObjectsInArray(halqaReports)
+                    break;
+                default:
+                    return this.sendResponse(req, res, {
+                        message: "Invalid area type!",
+                        status: 400,
+                    });
+            }
+
             return this.sendResponse(req, res, {
-                message: "Invalid area type!",
-                status: 400,
+                data: data,
+                status: 200,
+            });
+            
+        } catch (err) {
+            console.error(err);
+            return this.sendResponse(req, res, {
+                message: "Internal Server Error",
+                status: 500,
             });
         }
-      return this.sendResponse(req, res, {
-        data: data,
-        status: 200,
-    });
-      
-    } catch (err) {
-      console.error(err);
-      return this.sendResponse(req, res, {
-        message: "Internal Server Error",
-        status: 500,
-      });
-    }
-  };
+    };
 }
+
 module.exports = Compilation;
