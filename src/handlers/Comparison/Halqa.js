9b8931b22df9363ac1f5d90e883ccb2738ae7d72
Hassaan Mujtaba <saddiquehari2511@gmail.com>
2024-06-27 16:17:47 +0500

    partial

--- src/handlers/Comparison/Halqa.js
+++ src/handlers/Comparison/Halqa.js
@@ -38,6 +38,8 @@ class HalqaCompare extends Response {
     try {
       const token = req?.headers?.authorization;
       const { dates, areaId, duration_type } = req?.body;
+      const property = req?.params?.property;
+
       if (dates.length < 2) {
         return this.sendResponse(req, res, {
           message: "Atleast 2 dates required",
@@ -93,7 +95,34 @@ class HalqaCompare extends Response {
           },
           "wiId"
         ).populate("wiId");
-        if (report.length > 0) {
+        if (report.length > 0 && property === "spiderChart") {
+          const keys = Object.keys(report[report.length - 1].wiId._doc).filter(
+            (key) => key !== "_id" && key !== "__v"
+          );
+          keys.forEach((doc) => {
+            if (report[report.length - 1].wiId._doc[doc]) {
+              sample.data.push(
+                ((parseInt(
+                  report[report.length - 1].wiId._doc[doc]._doc.start
+                ) +
+                  parseInt(
+                    report[report.length - 1].wiId._doc[doc]._doc.increase
+                  ) -
+                  parseInt(
+                    report[report.length - 1].wiId._doc[doc]._doc.decrease
+                  )) /
+                  parseInt(
+                    report[report.length - 1].wiId._doc[doc]._doc.monthly
+                  )) *
+                  100
+              );
+              if (!labels.includes(doc.toLowerCase())) {
+                labels.push(doc.toLowerCase());
+              }
+            }
+          });
+        }
+        if (report.length > 0 && property !== "spiderChart") {
           const keys = Object.keys(report[report.length - 1].wiId._doc).filter(
             (key) => key !== "_id" && key !== "__v"
           );
@@ -132,6 +161,7 @@ class HalqaCompare extends Response {
     try {
       const token = req?.headers?.authorization;
       const { dates, areaId, duration_type } = req?.body;
+      const property = req?.params?.property;
       if (dates.length < 2) {
         return this.sendResponse(req, res, {
           message: "Atleast 2 dates required",
@@ -187,6 +217,30 @@ class HalqaCompare extends Response {
           },
           "halqaActivityId"
         ).populate("halqaActivityId");
+        if (reports.length > 0 && property === "spiderChart") {
+          const keys = Object.keys(
+            reports[reports?.length - 1]._doc.halqaActivityId._doc
+          ).filter((key) => key !== "_id" && key !== "__v");
+
+          keys.forEach((doc) => {
+            if (reports[reports?.length - 1]._doc?.halqaActivityId._doc[doc]) {
+              sample.data.push(
+                (parseInt(
+                  reports[reports?.length - 1]._doc.halqaActivityId._doc[doc]
+                    ._doc.decided
+                ) /
+                  parseInt(
+                    reports[reports?.length - 1]._doc.halqaActivityId._doc[doc]
+                      ._doc.completed
+                  )) *
+                  100
+              );
+              if (!labels.includes(doc.toLowerCase())) {
+                labels.push(doc.toLowerCase());
+              }
+            }
+          });
+        }
         if (reports?.length > 0) {
           const keys = Object.keys(
             reports[reports?.length - 1]._doc.halqaActivityId._doc
@@ -317,6 +371,7 @@ class HalqaCompare extends Response {
     try {
       const token = req?.headers?.authorization;
       const { dates, areaId, duration_type } = req?.body;
+      const property = req?.params?.property;
       if (dates.length < 2) {
         return this.sendResponse(req, res, {
           message: "Atleast 2 dates required",
@@ -372,8 +427,18 @@ class HalqaCompare extends Response {
           },
           "tdId"
         ).populate("tdId");
-
-        if (reports?.length > 0) {
+        if (reports.length > 0 && property === "spiderChart") {
+          if (reports[reports?.length - 1]._doc?.tdId._doc) {
+            sample.data.push(
+              ((parseInt(reports[reports?.length - 1]._doc?.tdId.meetings) /
+                parseInt(
+                  reports[reports?.length - 1]._doc?.tdId.rwabitMeetingsGoal
+                )) *
+                100).toFixed(2)
+            );
+          }
+          labels.push("meetings");
+        } else {
           const keys = Object.keys(
             reports[reports?.length - 1]._doc.tdId._doc
           ).filter((key) => key !== "_id" && key !== "__v");
@@ -390,6 +455,7 @@ class HalqaCompare extends Response {
         }
         datasets.push(sample);
       }
+      console.log(labels, datasets);
       response.data.labels = labels;
       response.data.datasets = datasets;
       return { labels, datasets };
@@ -499,6 +565,7 @@ class HalqaCompare extends Response {
     try {
       const token = req?.headers?.authorization;
       const { dates, areaId, duration_type } = req?.body;
+      const property = req?.params?.property;
       if (dates.length < 2) {
         return this.sendResponse(req, res, {
           message: "Atleast 2 dates required",
@@ -554,7 +621,8 @@ class HalqaCompare extends Response {
           },
           "rsdId"
         ).populate("rsdId");
-
+        if (reports?.length > 0 && property === "spiderChart") {
+        }
         if (reports?.length > 0) {
           const keys = Object.keys(
             reports[reports?.length - 1]._doc.rsdId._doc
@@ -756,6 +824,84 @@ class HalqaCompare extends Response {
       });
     }
   };
+  spiderComparison = async (req, res) => {
+    try {
+      const token = req?.headers?.authorization;
+      if (!token) {
+        return this.sendResponse(req, res, {
+          message: "Access Denied",
+          status: 400,
+        });
+      }
+
+      const decoded = jwt.decode(token.split(" ")[1]);
+      const userId = decoded?.id;
+      if (!userId) {
+        return this.sendResponse(req, res, {
+          message: "ID is required",
+          status: 403,
+        });
+      }
+
+      const { dates } = req.body;
+      if (dates.length < 2) {
+        return this.sendResponse(req, res, {
+          message: "Atleast 2 dates required",
+          status: 400,
+        });
+      }
+
+      const labels = [];
+      const datasets = [];
+
+      const reportFunctions = [
+        this.createIfradiQuawatReport,
+        this.createActivitiesReport,
+        this.toseeDawatReport,
+        this.rozShabBedari,
+        this.baitulmal,
+      ];
+
+      // Utility function to find a dataset with a specific label
+      const findDatasetByLabel = (label) =>
+        datasets.find((dataset) => dataset.label === label);
+
+      for (const reportFunction of reportFunctions) {
+        const { labels: reportLabels, datasets: reportDatasets } =
+          await reportFunction.call(this, req);
+
+        // Update labels
+        reportLabels.forEach((label) => {
+          if (!labels.includes(label)) {
+            labels.push(label);
+          }
+        });
+
+        // Update datasets
+        reportDatasets.forEach((reportDataset) => {
+          const existingDataset = findDatasetByLabel(reportDataset.label);
+          if (existingDataset) {
+            // If dataset with the same label exists, merge its data
+            existingDataset.data.push(...reportDataset.data);
+          } else {
+            // Otherwise, add the new dataset
+            datasets.push(reportDataset);
+          }
+        });
+      }
+
+      // Update response
+      response.data.labels = labels;
+      response.data.datasets = datasets;
+      res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      return this.sendResponse(req, res, {
+        message: "Internal Server Error",
+        status: 500,
+      });
+    }
+  };
   main = (req, res) => {
     const property = req?.params?.property;
     switch (property) {
@@ -779,6 +925,9 @@ class HalqaCompare extends Response {
         break;
       case "compareAll":
         this.halqaComparison(req, res);
+        break;
+      case "spiderChart":
+        this.spiderComparison(req, res);
         break;
       default:
         return this.sendResponse(req, res, {
