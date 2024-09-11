90eccd5d7c9e39794d169162940392c5926cf1bd
HassaanMujtaba <shuja0094@gmail.com>
2024-09-11 17:06:03 +0500

    forget pass

--- src/handlers/User.js
+++ src/handlers/User.js
@@ -460,6 +460,7 @@ class User extends Response {
           status: 400,
         });
       }
+
       const employeeExist = await UserModel.findOne({ email });
       if (employeeExist) {
         await ResetPasswordModel.deleteMany({ email });
@@ -468,41 +469,92 @@ class User extends Response {
         });
         const mailer = new Mailer();
         const html = `
-          <!DOCTYPE html>
-          <html>
-          <head>
-            <meta charset="UTF-8">
-            <title>Password Reset</title>
-          </head>
-          <body>
-            <table align="center" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
-                <tr>
-                    <td align="center" bgcolor="#0078d4" style="padding: 40px 0;">
-                        <h1 style="color: #fff;">Password Reset</h1>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="padding: 20px 0;">
-                        <p>Hello,</p>
-                        <p>You have requested to reset your password. To complete the password reset process, please click the following link:</p>
-                        <p><a href="${
-                          process.env.BASE_URL || "http://localhost:3000"
-                        }/reset?key=${key}" style="background-color: #0078d4; color: #fff; padding: 10px 20px; text-decoration: none;">Reset Password</a></p>
-                        <p>If you did not request a password reset, you can ignore this email.</p>
-                        <p>Thank you,</p>
-                        <p>JIR - ConsoleDot</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td bgcolor="#0078d4" style="padding: 20px 0; color: #fff; text-align: center;">
-                        &copy; 2023 ConsoleDot. All rights reserved.
-                    </td>
-                </tr>
-            </table>
-          </body>
-          </html>`;
+          
+<!doctype html>
+<html lang="en-US">
+
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>Reset Password Email Template</title>
+    <meta name="description" content="Reset Password Email Template.">
+    <style type="text/css">
+        a:hover {text-decoration: underline !important;}
+    </style>
+</head>
+
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    <!--100% body table-->
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td>
+                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                    align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                          <a href="https://www.jamiatreporting.com" title="logo" target="_blank">
+                            <img width="60" src="https://www.jamiatreporting.com/logo.png" title="logo" alt="Jamiat Reporting">
+                          </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+                                            requested to reset your password</h1>
+                                        <span
+                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                        <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                            We cannot simply send you your old password. A unique link to reset your
+                                            password has been generated for you. To reset your password, click the
+                                            following link and follow the instructions.
+                                        </p>
+                                        <a href="${
+                                          process.env.BASE_URL ||
+                                          "http://localhost:3000"
+                                        }/reset?key=${key}"
+                                            style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+                                            Password</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.jamiatreporting.com</strong></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <!--/100% body table-->
+</body>
+
+</html>`;
         mailer.sendMail(
-          process.env.SMTP_EMAIL,
+          process.env.MAIL_EMAIL,
           email,
           "Reset password link",
           html
@@ -1343,7 +1395,6 @@ class User extends Response {
       const token = req.headers.authorization;
       const decoded = jwt.decode(token.split(" ")[1]);
       const userId = decoded?.id;
-      console.log(userId);
       const file = req.files;
       const { mimetype, data, name } = file.profileImage;
 
@@ -1358,7 +1409,7 @@ class User extends Response {
       const updateUser = await UserModel.findByIdAndUpdate(userId, {
         profileImage: uploaded._id,
       });
-      console.log(uploaded, updateUser);
+      
       if (!updateUser) {
         return this.sendResponse(req, res, {
           status: 201,
